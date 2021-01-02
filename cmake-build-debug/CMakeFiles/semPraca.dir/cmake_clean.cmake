5c7fd7357a4c89504f37b23d548d0800c12235e7
ThePinzo <stefanculik82@gmail.com>
2021-01-02 13:15:50 +0100

    Zaciatok

---
+++ cmake-build-debug/CMakeFiles/semPraca.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/semPraca.dir/main.c.o"
+  "semPraca.pdb"
+  "semPraca"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/semPraca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ server.h
@@ -0,0 +1,10 @@
+//
+// Created by stefa on 2. 1. 2021.
+//
+
+#ifndef SEMPRACA_SERVER_H
+#define SEMPRACA_SERVER_H
+
+#endif //SEMPRACA_SERVER_H
+
+
