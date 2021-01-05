296b0dcc63cebcd9270047a44d4de321886d0920
ThePinzo <stefanculik82@gmail.com>
2021-01-05 12:50:46 +0100

    cleanup

--- client.h
+++ client.h
@@ -5,4 +5,21 @@
 #ifndef SEMPRACA_CLIENT_H
 #define SEMPRACA_CLIENT_H
 
+#define OPEN 0
+#define CD 1
+#define MKD 2
+#define RMD 3
+#define QUIT 4
+#define DIR 5
+#define GET 6
+#define PUT 7
+#define PWD 8
+#define DEL 9
+#define HELP 10
+
+
+
+
+
+
 #endif //SEMPRACA_CLIENT_H
